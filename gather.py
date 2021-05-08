34696977cdf5291bcd06e04593be5fdbb4a24fb3
Hessam <hessam2koucheki@gmail.com>
2021-05-08 13:03:01 +0430

    import RE and remove whitespaces

--- gather.py
+++ gather.py
@@ -1,5 +1,7 @@
+from os import makedev
 import requests
 from bs4 import BeautifulSoup
+import re
 
 # Load the whole webpage
 page = requests.get('https://dic.b-amooz.com/en/dictionary/w?word=best')
@@ -10,7 +12,8 @@ meanings = soup.find_all('a', attrs={'class' : 'quick-access-items'} )
 # print(val[2])
 
 for meaning in meanings:
-    print(meaning) 
+    # print(str(meaning)) 
+    print(re.sub(r'\s+', ' ', str(meaning)))
     print('------------------')
 
 # print(soup)
