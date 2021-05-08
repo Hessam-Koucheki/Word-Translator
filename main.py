c0d277e2b953a9c991db1e3feca8ea6576063899
Hessam <hessam2koucheki@gmail.com>
2021-05-08 19:05:42 +0430

    Prevent RETRANSLATE by reading and storing whole file

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ class Word:
         self.word = word
         self.meanings =meanings_list
 
+existing_words = []
 list_of_words = []
 
+# ReLoad All Existing words in file to prevent ReTranslate
+with open('WordLists.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        if not line.startswith('|'):
+            existing_words.append(line[:-3])
+
+# Main Loop to input Words to be translated
 while True:
+    # Input WORD
     word_input = input('Enter A Word To Translate: ')
     if word_input == '/exit':
         break
+    # Request To TRANSLATE Word
     tmp_meanings = gather.Translate(word_input)
+    # Store In a CLASS 
     tmp_word = Word(word_input, tmp_meanings)
+    # Append Class To the LIST
     list_of_words.append(tmp_word)
 
 
@@ -21,5 +34,5 @@ with open('WordLists.txt', 'a+') as file:
         file.writelines(this_word.word + ' :' + '\n')
         for this_mean in this_word.meanings:
             file.writelines('||\t' + this_mean + '\n')
-        file.writelines('-------------------------------\n')
-    file.close()
+        file.writelines('|-------------------------------\n')
+file.close()
