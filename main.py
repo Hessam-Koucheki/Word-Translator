23376c10e8aad2ebe4866582fc83f201bdb95c4b
Hessam <hessam2koucheki@gmail.com>
2021-05-08 18:52:13 +0430

    Save Words in a FILE

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import gather
 class Word:
     def __init__(self, word, meanings_list):
         self.word = word
-        self.meaning =meanings_list
+        self.meanings =meanings_list
 
 list_of_words = []
 
 while True:
     word_input = input('Enter A Word To Translate: ')
-    if word_input == 'exit':
+    if word_input == '/exit':
         break
     tmp_meanings = gather.Translate(word_input)
     tmp_word = Word(word_input, tmp_meanings)
     list_of_words.append(tmp_word)
 
 
-for t in list_of_words:    
-    print(t.meaning)
+with open('WordLists.txt', 'a+') as file:    
+    for this_word in list_of_words:
+        file.writelines(this_word.word + ' :' + '\n')
+        for this_mean in this_word.meanings:
+            file.writelines('||\t' + this_mean + '\n')
+        file.writelines('-------------------------------\n')
+    file.close()
