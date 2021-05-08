c58c1bd817f6d65a47a762878982f5891feaf897
Hessam <hessam2koucheki@gmail.com>
2021-05-08 18:16:53 +0430

    Store Translated Words in a list

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import gather
 
 class Word:
-    def __init__(self, data):
-        self.word = data
+    def __init__(self, word, meanings_list):
+        self.word = word
+        self.meaning =meanings_list
+
+list_of_words = []
+
+while True:
+    word_input = input('Enter A Word To Translate: ')
+    if word_input == 'exit':
+        break
+    tmp_meanings = gather.Translate(word_input)
+    tmp_word = Word(word_input, tmp_meanings)
+    list_of_words.append(tmp_word)
 
 
-word_input = input()
-
-gather.Translate(word_input)
+for t in list_of_words:    
+    print(t.meaning)
