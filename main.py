31daf448ad3d8c5a5e43fabea5a28bed2d6cd665
Hessam <hessam2koucheki@gmail.com>
2021-05-08 19:16:24 +0430

    Remove Class, Existing_words[], avoid duplication, write as input

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
 import gather
 
-class Word:
-    def __init__(self, word, meanings_list):
-        self.word = word
-        self.meanings =meanings_list
-
 existing_words = []
-list_of_words = []
 
 # ReLoad All Existing words in file to prevent ReTranslate
 with open('WordLists.txt', 'r') as file:
     lines = file.readlines()
     for line in lines:
         if not line.startswith('|'):
-            existing_words.append(line[:-3])
+            existing_words.append(line[:-3].strip())
 
 # Main Loop to input Words to be translated
-while True:
-    # Input WORD
-    word_input = input('Enter A Word To Translate: ')
-    if word_input == '/exit':
-        break
-    # Request To TRANSLATE Word
-    tmp_meanings = gather.Translate(word_input)
-    # Store In a CLASS 
-    tmp_word = Word(word_input, tmp_meanings)
-    # Append Class To the LIST
-    list_of_words.append(tmp_word)
-
-
 with open('WordLists.txt', 'a+') as file:    
-    for this_word in list_of_words:
-        file.writelines(this_word.word + ' :' + '\n')
-        for this_mean in this_word.meanings:
+    while True:
+        # Input WORD
+        word_input = input('Enter A Word To Translate: ').strip()
+        if word_input == '/exit':
+            break
+        elif word_input in existing_words:
+            print(word_input.upper() + ' exists!!')
+            continue
+        # Request To TRANSLATE Word
+        tmp_meanings = gather.Translate(word_input)
+        # Store In a FILE
+        file.writelines(word_input + ' :' + '\n') # Write WORD
+        for this_mean in tmp_meanings: # Write MEANINGS
             file.writelines('||\t' + this_mean + '\n')
         file.writelines('|-------------------------------\n')
+        existing_words.append(word_input.strip()) # Add to Existing List to avoid duplication
+
+
 file.close()
