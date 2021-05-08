7a98f8a46f3de97cb1bba06542f8ca7b10d849a7
Hessam <hessam2koucheki@gmail.com>
2021-05-08 17:49:59 +0430

    Translate input word

--- gather.py
+++ gather.py
@@ -1,23 +1,24 @@
-from os import makedev
 import requests
 from bs4 import BeautifulSoup
 import re
 
-# Load the whole webpage
-page = requests.get('https://dic.b-amooz.com/en/dictionary/w?word=best')
-soup = BeautifulSoup(page.text, 'html.parser')
+def Translate(Word):
+    Url = 'https://dic.b-amooz.com/en/dictionary/w?word=' + Word
+    # Load the whole webpage
+    page = requests.get(Url)
+    soup = BeautifulSoup(page.text, 'html.parser')
 
-# Grab appropriate section using quick access menu
-meanings = soup.find_all('a', attrs={'class' : 'quick-access-items'} )
-# print(val[2])
+    # Grab appropriate section using quick access menu
+    meanings = soup.find_all('a', attrs={'class' : 'quick-access-items'} )
 
-for meaning in meanings: 
-    meaning = str(meaning)
-    meaning = re.sub(r'\s+', ' ', meaning) # remove whitespaces
-    meaning = re.sub(r'<.+?>', ' ', meaning) # remove tags and spans
-    meaning = re.sub(r'\d+', ' ', meaning) # remove numbers from start
-    meaning = re.sub('\.', ' ', meaning) # remove dot from start
-    meaning = meaning.strip() # remove any extra left whitespaces
-    print(meaning)
+    for meaning in meanings: 
+        meaning = str(meaning)
+        meaning = re.sub(r'\s+', ' ', meaning) # remove whitespaces
+        meaning = re.sub(r'<.+?>', ' ', meaning) # remove tags and spans
+        meaning = re.sub(r'\d+', ' ', meaning) # remove numbers from start
+        meaning = re.sub('\.', ' ', meaning) # remove dot from start
+        meaning = meaning.strip() # remove any extra left whitespaces
+        print(meaning)
 
-# print(soup)
+
+# Translate('love')
--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import gather
+
 class Word:
     def __init__(self, data):
         self.word = data
+
+
+word_input = input()
+
+gather.Translate(word_input)
