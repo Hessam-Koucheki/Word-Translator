60ea459af3982a18a434abcc2c33d59443549792
Hessam <hessam2koucheki@gmail.com>
2021-05-11 17:31:16 +0430

    place file to Desktop, Add command /OPEN

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gather
 from os import system
-wordlist_address = '/mnt/g/Codes/Python/Trasnlator/WordLists.txt'
+# wordlist_address = '/mnt/g/Codes/Python/Trasnlator/WordLists.txt'
+wordlist_address = '/mnt/c/Users/Hessam/Desktop/WordLists.txt'
 
 # ReLoad All Existing words in file to prevent ReTranslate
 existing_words = []
@@ -29,7 +30,9 @@ with open(wordlist_address, 'a+') as file:
             system('clear || cls')
             existing_words = []
             print('All file contents have been erased!!')
-
+        elif word_input == '/open':
+            command = 'notepad.exe ' + r'C:\\Users\\Hessam\\Desktop\\WordLists.txt'
+            system(command)
         elif word_input in existing_words:
             print(word_input.upper() + ' exists!!')
             continue
