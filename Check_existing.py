f24f874b7cfb20f3f34a3af8af39717010215ad4
Hessam <hessam2koucheki@gmail.com>
2021-05-26 00:33:34 +0430

    Function CHECK_EXISTING

---
+++ Check_existing.py
@@ -0,0 +1,17 @@
+from addresses import *
+
+
+# ReLoad All Existing words in file to prevent ReTranslate
+existing_words = []
+def check_existing():
+    try:
+        with open(wordlist_address_windows, 'r+', encoding='utf-8') as file:
+            lines = file.readlines()
+            for line in lines:
+                if not line.startswith('|'):
+                    existing_words.append(line[:-3].strip())
+        file.close()
+    except:
+        print('No File Already Exists, Creating...')
+        open(wordlist_address_windows, 'w+', encoding='utf-8').close()
+    return existing_words
