fe602719da8ffe70cc6baa14aed3d9986a587e1c
Hessam <hessam2koucheki@gmail.com>
2021-05-26 00:33:17 +0430

    add file ADDRESSES

---
+++ addresses.py
@@ -0,0 +1,4 @@
+
+wordlist_address_linux = '/mnt/c/Users/Hessam/Desktop/WordLists.txt' # wsl format
+wordlist_address_windows = r'C:\\Users\\Hessam\\Desktop\\WordLists.txt' # windows format
+# wordlist_address_windows = 'notepad.exe ' + r'C:\\Users\\Hessam\\Desktop\\WordLists.txt' # windows format
