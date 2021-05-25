0acee9ee9a5c0b6af0c6ea7764a6b0f9a2dbd6ef
Hessam <hessam2koucheki@gmail.com>
2021-05-25 22:34:19 +0430

    Move import files to tran.py from main.py

--- tran.py
+++ tran.py
@@ -9,6 +9,24 @@
 
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+import gather
+from os import system
+wordlist_address = '/mnt/c/Users/Hessam/Desktop/WordLists.txt' # wsl format
+notepad_command = 'notepad.exe ' + r'C:\\Users\\Hessam\\Desktop\\WordLists.txt' # windows format
+
+# ReLoad All Existing words in file to prevent ReTranslate
+existing_words = []
+try:
+    with open(wordlist_address, 'r+') as file:
+        lines = file.readlines()
+        for line in lines:
+            if not line.startswith('|'):
+                existing_words.append(line[:-3].strip())
+    file.close()
+except:
+    print('No File Already Exists, Creating...')
+    open(wordlist_address, 'w+').close()
+
 
 
 class Ui_MainWindow(object):
